b8f4c1889a353a2c9dd78e1bd66fe8ee520d693a
Silvan Wegmann <swegmann@narf.ch>
2014-08-25 22:56:17 +0200

    Discover Python executable instead of hardcoding it

--- bin/dev_shell.bat
+++ bin/dev_shell.bat
@@ -4,5 +4,23 @@ REM correctly such that the 'hi' commands work
 
 set PATH=%PATH%;%~dp0
 
+for /F "usebackq tokens=*" %%a in (`where py`) do SET PYTHON_EXE=%%a
+if defined PYTHON_EXE GOTO PythonFound
+
+if exist C:\Python32\python.exe (
+    set PYTHON_EXE=C:\Python32\python.exe
+    GOTO PythonFound
+)
+
+GOTO PythonNotFound
+
+:PythonFound
 cd %~dp0\..
 cmd.exe
+GOTO ScriptEnd
+
+:PythonNotFound
+echo "The Python executable could not be found. Make sure it is installed"
+pause
+
+:ScriptEnd
--- bin/hi.bat
+++ bin/hi.bat
@@ -1,5 +1,5 @@
 @echo off
 set HI_PATH=%~dp0\..\scripts\hi.py
 for %%h IN (%HI_PATH%) DO (
-    c:\python32\python.exe %%~fh %*
+    %PYTHON_EXE% %%~fh %*
 )
